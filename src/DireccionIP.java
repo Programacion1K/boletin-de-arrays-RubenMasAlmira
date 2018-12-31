e4413f551f2a35f6d70547f0f412c5f9070a93ff
Rubén Más Almira <rubenmasalmira@gmail.com>
2018-12-31 20:01:40 +0100

    Resuelto el método erroneo en la clase prueba y implementado en DireccionIP

--- src/DireccionIP.java
+++ src/DireccionIP.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class DireccionIP {
 
-/*  - La clase (A, B, C) a la que pertenece la IP
-    - Si es una Id de Red
-    - La Id de Red de esa Dirección IP
-    - La máscara de red.
-    - Si es privada o pública  */
-    private static final int LONGITUD_DE_LA_IP=4;
-    private int[]IP=new int[LONGITUD_DE_LA_IP];
+    /*  - La clase (A, B, C) a la que pertenece la IP
+        - Si es una Id de Red
+        - La Id de Red de esa Dirección IP
+        - La máscara de red.
+        - Si es privada o pública  */
+    private static final int LONGITUD_DE_LA_IP = 4;
+    private int[] IP = new int[LONGITUD_DE_LA_IP];
     private char clase;
     private String red;
     private int[] IPDeRed;
@@ -16,41 +16,40 @@ public class DireccionIP {
     private String PrivadaPublica;
     //todo hay que completar los constructores con los demas datos
 
-    DireccionIP(int[]IP){
-        this.IP=IP;
+    DireccionIP(int[] IP) {
+        this.IP = IP;
 
     }
 
-    DireccionIP(int primerOcteto,int segundoOcteto,int tercerOcteto,int cuartoOcteto){
-        IP[0]=primerOcteto;
-        IP[1]=segundoOcteto;
-        IP[2]=tercerOcteto;
-        IP[3]=cuartoOcteto;
+    DireccionIP(int primerOcteto, int segundoOcteto, int tercerOcteto, int cuartoOcteto) {
+        IP[0] = primerOcteto;
+        IP[1] = segundoOcteto;
+        IP[2] = tercerOcteto;
+        IP[3] = cuartoOcteto;
     }
 
-    DireccionIP(String IPASeparar){
-        System.out.println(IP = convertirStringEnArrayDeInt(IPASeparar));
+    DireccionIP(String IPASeparar) {
+        IP = convertirStringEnArrayDeInt(IPASeparar);
     }
+
     //ERROR
-    public static String infoIP(DireccionIP IP){
+    public static String infoIP(DireccionIP IP) {
         return IP.getIP();
     }
+
     //ERROR
-    public String getIP(){
+    public String getIP() {
         return Arrays.toString(IP);
     }
 
     //Metodo para convertir un String en un array de enteros
-    private int[] convertirStringEnArrayDeInt(String IPASeparar){
-        int[] IP=new int[LONGITUD_DE_LA_IP];
-        int IPIntroducidas=0;
-        int posicionDelUltimoPunto=0;
-        for(int i=0;i<IPASeparar.length()-1;i++){
-            for(int j=0;j<IP.length-1;j++){
-                if(IPASeparar.charAt(i)=='.'){
-                    IP[j]=Integer.parseInt(IPASeparar.substring(posicionDelUltimoPunto,i));
-                    posicionDelUltimoPunto=i;
-                }
+    private static int[] convertirStringEnArrayDeInt(String IPASeparar) {
+        int[] IP = new int[4];
+        for (int i = 0; i < 3; i++) {
+            IP[i] = Integer.parseInt(IPASeparar.substring(0, IPASeparar.indexOf('.')));
+            IPASeparar = IPASeparar.substring(IPASeparar.indexOf('.') + 1, IPASeparar.length());
+            if (IPASeparar.indexOf('.') == -1) {
+                IP[3] = Integer.parseInt(IPASeparar);
             }
         }
         return IP;
--- src/Prueba.java
+++ src/Prueba.java
@@ -4,12 +4,18 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola, Mundo");
         int[] IP=convertirStringEnArrayDeInt("192.168.10.2");
-        System.out.println(Arrays.toString(IP));
+        //System.out.println(Arrays.toString(IP));
 
     }
     private static int[] convertirStringEnArrayDeInt(String IPASeparar){
         int[] IP=new int[4];
-
+        for(int i=0;i<3;i++){
+            IP[i]=Integer.parseInt(IPASeparar.substring(0,IPASeparar.indexOf('.')));
+            IPASeparar=IPASeparar.substring(IPASeparar.indexOf('.')+1,IPASeparar.length());
+            if(IPASeparar.indexOf('.')==-1){
+                IP[3]=Integer.parseInt(IPASeparar);
+            }
+        }
         /* int IPIntroducidas=0;
         int posicionDelUltimoPunto=0;
         int j=0;
