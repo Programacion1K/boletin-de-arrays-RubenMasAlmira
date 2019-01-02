720dc6fad7c44edd4b9c331b07fb9cd72c15f624
Rubén Más Almira <rubenmasalmira@gmail.com>
2019-01-02 22:02:28 +0100

    Continuacion del ejercicio de las IP

--- src/DireccionIP.java
+++ src/DireccionIP.java
@@ -8,28 +8,25 @@ public class DireccionIP {
         - La máscara de red.
         - Si es privada o pública  */
     private static final int LONGITUD_DE_LA_IP = 4;
-    private int[] IP = new int[LONGITUD_DE_LA_IP];
-    private char clase;
-    private String red;
-    private int[] IPDeRed;
-    private int[] MascaraDeRed;
-    private String PrivadaPublica;
-    //todo hay que completar los constructores con los demas datos
+    private  int[] IP = new int[LONGITUD_DE_LA_IP];
 
     DireccionIP(int[] IP) {
         this.IP = IP;
 
+
     }
 
     DireccionIP(int primerOcteto, int segundoOcteto, int tercerOcteto, int cuartoOcteto) {
-        IP[0] = primerOcteto;
-        IP[1] = segundoOcteto;
-        IP[2] = tercerOcteto;
-        IP[3] = cuartoOcteto;
+        this.IP[0] = primerOcteto;
+        this.IP[1] = segundoOcteto;
+        this.IP[2] = tercerOcteto;
+        this.IP[3] = cuartoOcteto;
+
     }
 
     DireccionIP(String IPASeparar) {
-        IP = convertirStringEnArrayDeInt(IPASeparar);
+        this.IP = convertirStringEnArrayDeInt(IPASeparar);
+
     }
 
     //ERROR
@@ -39,12 +36,13 @@ public class DireccionIP {
 
     //ERROR
     public String getIP() {
+
         return Arrays.toString(IP);
     }
 
-    //Metodo para convertir un String en un array de enteros
+    //Método para convertir un String en un array de enteros
     private int[] convertirStringEnArrayDeInt(String IPASeparar) {
-        int[] IP = new int[4];
+        int[] IP = new int[LONGITUD_DE_LA_IP];
         for (int i = 0; i < 3; i++) {
             IP[i] = Integer.parseInt(IPASeparar.substring(0, IPASeparar.indexOf('.')));
             IPASeparar = IPASeparar.substring(IPASeparar.indexOf('.') + 1, IPASeparar.length());
@@ -54,4 +52,40 @@ public class DireccionIP {
         }
         return IP;
     }
+
+    //Método para obtener la IdDeRed
+    private int[] obtenerIdDeRed(int[]IP){
+        int[] IdDeRed = new int[LONGITUD_DE_LA_IP];
+        IdDeRed=IP.clone();
+        IdDeRed[1]=0;
+        IdDeRed[2]=0;
+        IdDeRed[3]=0;
+        return IdDeRed;
+    }
+
+    //Método para obtener la clase
+    private char obtenerClase(int[]IP){
+        char clase;
+        if(IP[0]>=0 && IP[0]<=127){
+            return 'A';
+        }else if(IP[0]>=128 && IP[0]<=191){
+            return 'B';
+        }else if(IP[0]>=192 && IP[0]<=223){
+            return 'C';
+        }
+        return 'N';
+    }
+    //Método para obtener la Mascara de Red
+    private int[] obtenerMascara(int[] IP){
+        if (obtenerClase(IP)=='A'){
+            return
+        }
+        if(obtenerClase(IP)=='B'){
+            return
+        }
+        if(obtenerClase(IP)=='C'){
+            return
+        }
+    }
+
 }
