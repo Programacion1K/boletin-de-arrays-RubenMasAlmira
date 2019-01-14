ece6a19cea047aa8d2957e67b193f55424f94b1c
Rubén Más Almira <rubenmasalmira@gmail.com>
2019-01-14 13:02:30 +0100

    Terminación del programa interactivo

--- src/DireccionIpInteractivo.java
+++ src/DireccionIpInteractivo.java
@@ -1,30 +1,64 @@
 import javax.swing.*;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-
 public class DireccionIpInteractivo {
     public static void main(String[] args) {
-        String[]formasParaIntroducirIP={"Cadena Normal","Cuatro bytes","Salir"};
-        Icon iconoIP=new ImageIcon(("C:\\Users\\Usuario\\IdeaProjects\\boletin-de-arrays-RubenMasAlmira\\src"));
-        int opcionParaIntroducirIP= (int)JOptionPane.showOptionDialog(null,
-                "Elija un método para introducir la IP",
-                "Introducir IP",
-                JOptionPane.PLAIN_MESSAGE,
-                JOptionPane.PLAIN_MESSAGE,
-                iconoIP,
-                formasParaIntroducirIP,null
-                );
-/*        switch (opcionParaIntroducirIP){
-            case 0:
-                String ipEnString=(String)JOptionPane.showInputDialog(null,
-                        "Introduzca la IP separada por puntos",
-                        "Entrada",
-                        JOptionPane.QUESTION_MESSAGE,
-                        null,null,null);
-                DireccionIP ipPorString=new DireccionIP(ipPorString);
-                JOptionPane.showMessageDialog(null,
-                        ipPorString.infoIP());
-                break;
-        }*/
+        boolean salida = false;
+        do {
+            //Pregunta inicial
+            String[] formasParaIntroducirIP = {"Cadena Normal", "Cuatro bytes", "Salir"};
+            Icon iconoIP = new ImageIcon(("iconop.png"));
+            int opcionParaIntroducirIP = (int) JOptionPane.showOptionDialog(null,
+                    "Elija un método para introducir la IP",
+                    "Introducir IP",
+                    JOptionPane.PLAIN_MESSAGE,
+                    JOptionPane.PLAIN_MESSAGE,
+                    new ImageIcon("./src/iconoIp.png"),
+                    formasParaIntroducirIP, null
+            );
+            //Elección según la respuesta dada en la pregunta inicial
+            switch (opcionParaIntroducirIP) {
+                case 0:
+                    String ipEnString = (String) JOptionPane.showInputDialog(null,
+                            "Introduzca la IP separada por puntos",
+                            "Entrada",
+                            JOptionPane.QUESTION_MESSAGE);
+                    DireccionIP ipPorString = new DireccionIP(ipEnString);
+                    JOptionPane.showMessageDialog(null,
+                            ipPorString.infoIP(),
+                            "INFORMACIÓN DE LA IP",
+                            JOptionPane.PLAIN_MESSAGE,
+                            new ImageIcon("./src/iconoIp.png"));
+                    break;
+                case 1:
+                    int[] cuatroBytes = new int[4];
+                    for (int i = 0; i < 4; i++) {
+                        int byteActual = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                "Introduzca el byte:" + i,
+                                "Entrada",
+                                JOptionPane.QUESTION_MESSAGE));
+                        cuatroBytes[i] = byteActual;
+                    }
+                    DireccionIP ipEnCuatroBytes = new DireccionIP(cuatroBytes);
+                    JOptionPane.showMessageDialog(null,
+                            ipEnCuatroBytes.infoIP(),
+                            "INFORMACIÓN DE LA IP",
+                            JOptionPane.PLAIN_MESSAGE,
+                            new ImageIcon("./src/iconoIp.png"));
+                    break;
+                default:
+                    String[] opcionesDeSalida = {"Sí", "No"};
+                    int salir = (int) JOptionPane.showOptionDialog(null,
+                            "¿Esta seguro de que quiere salir?",
+                            "Salida",
+                            JOptionPane.PLAIN_MESSAGE,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null,
+                            opcionesDeSalida,
+                            null);
+                    if(salir==0){
+                        salida=true;
+                    }
+
+            }
+        }while(salida!=true);
     }
 }
